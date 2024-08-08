a7107efc759f9e436bb9a85ac58e7c245fbb1bbe
0renzo0loli0 <u20161c993@upc.edu.pe>
2024-08-07 22:10:10 -0500

    fix(frontend): fix error type response from user services

--- frontend/src/services/user.service.ts
+++ frontend/src/services/user.service.ts
@@ -1,12 +1,15 @@
 import { User } from "@/model/user.entity";
 import http from "./http";
+import type { AxiosResponse } from "axios";
+
+type PromiseResponse<T> = Promise<AxiosResponse<T>>;
 
 export class UserApiService {
-  getUsers(): Promise<User[]> {
+  getUsers(): PromiseResponse<User[]> {
     return http.get("/auth/users");
   }
 
-  getUserById(id: string): Promise<User> {
+  getUserById(id: string): PromiseResponse<User> {
     return http.get("/user?id=" + id);
   }
 }
