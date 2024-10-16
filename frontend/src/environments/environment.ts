ba5495d33fb7380a1d401e1da22f0f7d4774679b
0renzo0loli0 <u20161c993@upc.edu.pe>
2024-10-16 18:21:29 -0500

    chore(frontend): environment fron env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
+const FRONTEND_API_URL = process.env['FRONTEND_API_URL'];
+
 export const environment = {
   production: true,
-  //improve this for production
-  apiUrl: 'http://127.0.0.1:3000/api/v1',
+  apiUrl: FRONTEND_API_URL || 'http://127.0.0.1:3000/api/v1',
 };
